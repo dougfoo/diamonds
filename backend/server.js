31b34f1b2d5fab5a34dfa24e757513efd3e504ef
dougfoo <doug.cha@gmail.com>
2019-09-26 17:20:24 -0400

    fix url for fetcher to host

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,13 @@ let Diamond = require('./diamond.model.js');
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://foo_ro:foo123@ds145916.mlab.com:45916/diamonds', { useNewUrlParser: true, useUnifiedTopology: true  });
-//mongoose.connect('mongodb://ds145916.mlab.com:45916/diamonds', { useNewUrlParser: true, auth: { user: DB_USER, password: DB_PW});
-const connection = mongoose.connection;
+// mongoose.connect('mongodb://foo_ro:foo123@ds145916.mlab.com:45916/diamonds', { useNewUrlParser: true, useUnifiedTopology: true  });
+// //mongoose.connect('mongodb://ds145916.mlab.com:45916/diamonds', { useNewUrlParser: true, auth: { user: DB_USER, password: DB_PW});
+// const connection = mongoose.connection;
 
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-})
+// connection.once('open', function() {
+//     console.log("MongoDB database connection established successfully");
+// })
 
 diamondRoutes.route('/').get(function(req, res) {
     Diamond.find( {price: { $gte: 200000 }},  function(err, diamonds) {
