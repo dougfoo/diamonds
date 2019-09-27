2d49fdbe1364a2e81fb2af3e1ba344aa542a1d10
dougfoo <doug.cha@gmail.com>
2019-09-26 20:42:59 -0400

    added build folder

---
+++ diamond-app/build/precache-manifest.0a1a7f46eab5bd076e92b49394827b48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3c08ac873b52113e0244a69d70f310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a003f0cb9f15f289a499",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "459eae81ff70655b6bae",
+    "url": "/static/js/2.5a2cbad7.chunk.js"
+  },
+  {
+    "revision": "a003f0cb9f15f289a499",
+    "url": "/static/js/main.c523c2a2.chunk.js"
+  },
+  {
+    "revision": "6a548eda2ff5f1196220",
+    "url": "/static/js/runtime-main.d03f0b7a.js"
+  }
+]);
